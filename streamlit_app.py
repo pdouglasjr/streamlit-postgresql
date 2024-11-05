1bf24233f1d773599f3f1f2b44eb9ce538d38145
Phillip Douglas <pdouglasjr@proton.me>
2024-11-05 00:06:33 -0500

    add functionality to refresh postgresql pets table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,39 @@
 import streamlit as st
+from sqlalchemy import text
 
-# intialize connection
+# Intialize connection
 conn = st.connection(
     name="postgres",
-    type="sql",
-    url="postgresql://pdouglasjr:#10EarlinE27#@localhost:5432/pets_db"
+    type="sql"
 )
 
+# Insert some data with conn.session
+with conn.session as s:
+    # clean database
+    s.execute(text("DROP TABLE IF EXISTS pets;"))
+    s.execute(text("CREATE TABLE pets (name TEXT, pet TEXT);"))
+    s.commit()
+
+    # data
+    pet_owners = {
+        'Mary': 'dog',
+        'John': 'cat',
+        'Robert': 'bird',
+        'Jerry': 'fish', 
+        'Barbara': 'cat',
+        'Alex': 'dog'
+    }
+
+    # add data to database
+    for k in pet_owners:
+        s.execute(
+            text('INSERT INTO pets (name, pet) VALUES (:name, :pet);'),
+            params=dict(name=k, pet=pet_owners[k])
+        )
+    s.commit()
+
 # Perform query
-df = conn.query('SELECT * FROM pets;', ttl="10m")
+df = conn.query('SELECT * FROM pets')
 
 # Print results
 for row in df.itertuples():
